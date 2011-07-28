0db172e6a369b6f2654f493768f0885571a2e923
Benjamin Aschenbrenner <benjamin.aschenbrenner@gmail.com>
2011-07-28 15:44:41 +0200

    initial commit of splitted thesis

---
+++ thesisBenni/del-temporary-files.bat
@@ -0,0 +1,4 @@
+rem This batch file deletes all temporary files
+rem Only for Windows
+
+del *.acn *.acr *.alg *.aux *.bbl *.blg *.dvi *.glg *.glo *.gls *.ist *.lof *.log *.lol *.lot *.out *.pdf *.toc
