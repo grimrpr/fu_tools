38565efcffd979781da303468cd935b6eccc3401
Simon Schmitt <mail@simonschmitt.com>
2011-08-08 14:37:32 +0200

    extending glos; finishing waypoint driver; starting verfStrg

--- waypoint/src/waypoint_driver.cpp
+++ waypoint/src/waypoint_driver.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include <ros/package.h>
 #include "waypoint/GetTracks.h"
 #include "waypoint/GetRoutes.h"
 #include "waypoint/GenerateTrack.h"
@@ -8,7 +9,39 @@
 #include "waypoint/DeleteTrack.h"
 #include "waypoint/DeleteRoute.h"
 #include <sstream>
+#include <fstream>
+#include <stdio.h>
 
+std::string storagePath;
+
+bool fileExists(const std::string &fileName)
+{
+  std::fstream f;
+
+  f.open(fileName.c_str(),std::ios::in);
+  if( f.is_open() )
+  {
+    f.close();
+    return true;
+  }
+  f.close();
+  return false;
+}
+
+std::string formatTime(const unsigned int sec)
+{
+  
+  std::stringstream t;
+
+  /*
+  unsigned int min    = sec / 60;
+  unsigned int hours  = min / 60;
+  unsigned int days   = hours / 24;
+  */
+
+  return t.str();
+  
+}
 bool getTracks(waypoint::GetTracks::Request &req, waypoint::GetTracks::Response &res)
 {
   ROS_INFO("getTracks called");
@@ -16,7 +49,8 @@ bool getTracks(waypoint::GetTracks::Request &req, waypoint::GetTracks::Response 
 	std::vector<waypoint::Track> vec;
 	waypoint::Track track;
 	track.name="Hans";
-	track.routeCount=2;
+  track.time=ros::Time::now();
+	vec.push_back(track);
 	vec.push_back(track);
 
 	res.tracks=vec;
@@ -56,6 +90,21 @@ bool runTrack(waypoint::RunTrack::Request &req, waypoint::RunTrack::Response &re
 bool deleteTrack(waypoint::DeleteTrack::Request &req, waypoint::DeleteTrack::Response &res)
 {
 	ROS_INFO("deleteTrack called");
+
+  std::stringstream fileName;
+  fileName << req.track.time.toSec() << "_t_" << req.track.name;
+  ROS_INFO(fileName.str().c_str());
+
+  if(fileExists(fileName.str()))
+  {
+    remove(fileName.str().c_str());
+    ROS_INFO("track removed");
+    res.successfull = true;
+    return true;
+  }
+
+  ROS_INFO("track not found");
+  res.successfull = false;
 	return true;
 }
 
@@ -70,6 +119,10 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "waypoint_driver");
 	ros::NodeHandle n;
 
+  std::stringstream path;
+  path << ros::package::getPath("waypoint") << "/storage/";
+  storagePath = path.str();
+  
 	ros::ServiceServer srvGetTracks     	= n.advertiseService("GetTracks", getTracks);
 	ros::ServiceServer srvGetRoutes     	= n.advertiseService("GetRoutes", getRoutes);
 	ros::ServiceServer srvGenerateTrack	  = n.advertiseService("GenerateTrack", generateTrack);
